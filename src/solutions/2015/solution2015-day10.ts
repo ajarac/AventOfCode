8fbe0180fb7b0b1c6dd65906d6c5a05cdd955f36
Angel Jara <ajarac92@gmail.com>
2022-06-22 09:10:19 +0400

    2015-10
    C

---
+++ src/solutions/2015/solution2015-day10.ts
@@ -0,0 +1,44 @@
+import { Solution } from '../../utils/solution';
+
+export class Solution2015Day10 extends Solution {
+    constructor() {
+        super(2015, 10);
+    }
+
+    private static buildArrayNumbers(data: string): number[] {
+        return data.trim().split('').map(Number);
+    }
+
+    solvePart1(data: string): string {
+        return this.processNTimes(Solution2015Day10.buildArrayNumbers(data), 40).toString();
+    }
+
+    solvePart2(data: string): string {
+        return this.processNTimes(Solution2015Day10.buildArrayNumbers(data), 50).toString();
+    }
+
+    private processNTimes(list: number[], times: number): number {
+        for (let i = 0; i < times; i++) {
+            list = this.process(list);
+        }
+        return list.length;
+    }
+
+    private process(list: number[]): number[] {
+        let result = [];
+        let index = 0;
+
+        while (index < list.length) {
+            let counter = 1;
+            while (index < list.length - 1 && list[index] === list[index + 1]) {
+                counter++;
+                index++;
+            }
+            result.push(counter);
+            result.push(list[index]);
+            index++;
+        }
+
+        return result;
+    }
+}
--- src/utils/solutions.ts
+++ src/utils/solutions.ts
@@ -1,7 +1,7 @@
-import { Solution2015Day9 } from '../solutions/2015/solution2015-day9';
+import { Solution2015Day10 } from '../solutions/2015/solution2015-day10';
 
 export function getAllSolutions() {
     return [
-        new Solution2015Day9()
+        new Solution2015Day10()
     ];
 }
